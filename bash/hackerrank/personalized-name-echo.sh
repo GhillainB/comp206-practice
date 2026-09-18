b01bd9e2141fec516addd96d55348fb1464e02cb
GhillainB <143177015+GhillainB@users.noreply.github.com>
2026-09-17 22:59:14 -0400

    add exercise info and reflection
    Added Exercise-Learned today-Next sections

--- bash/hackerrank/personalized-name-echo.sh
+++ bash/hackerrank/personalized-name-echo.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+
+#Exercise: Personalized echo
+#https:https://www.hackerrank.com/challenges/bash-tutorials---a-personalized-echo/problem?isFullScreen=true
+#Task: Write a Bash script which accepts  as input and displays the greeting "Welcome (name)"
+#Input: name
+#Output: Welcome "name"
+
+#Learned today: 
+# The sign $ doesn't require brackets () or " ", everything in immediate approximity are processed
+# read only requires variable name "x" or "y", the valuable will immediately be initialized hence $x = value
+
+#Next:
+#->Check if the sign $ can process a longer command not just proximity e.g $(date +%Y-%m--%dT%H:%M:%S) in
+# tar -czf backups/homework-$(date +%Y-%m-%dT%H:%M:%S).tar.gz  ~/comp-206/homework
+# since in shell no bracket is needed
+# say --> echo "Welcome $(name number)" or $name $number
+
 read name
 echo "Welcome $name"

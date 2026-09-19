f3cae7b92556f0ac46f803e082eb6d50c2d3b705
GhillainB <143177015+GhillainB@users.noreply.github.com>
2026-09-18 21:00:18 -0400

    final update
    addition of a commit components

--- bash/hackerrank/looping-with-numbers.sh
+++ bash/hackerrank/looping-with-numbers.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+#Exercise: Looping with numbers
+#https: https://www.hackerrank.com/challenges/bash-tutorials---looping-with-numbers/problem?isFullScreen=true
+#Task : Use a for loop to display the natural numbers from 1 to 50.
+#Input : None
+#Output: 1, 2, 3, 4 ... 50
+
+#Learned today:
+#-> for use double brackets = for ((...;...;...))
+
 for ((i=1; i<=50; i++)); do
     echo "$i"
 done
-

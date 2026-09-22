f05bb154eab98029bdc3bc6efe2b5ad24acce048
GhillainB <143177015+GhillainB@users.noreply.github.com>
2026-09-22 15:28:07 -0400

    single if bracket works

--- bash/hackerrank/comparing-numbers.sh
+++ bash/hackerrank/comparing-numbers.sh
@@ -2,11 +2,11 @@
 
 read x
 read y
-if [[ $x -lt $y ]]; then
+if [ $x -lt $y ]; then
     echo "X is less than Y"
-elif [[ $x -gt $y ]]; then
+elif [ $x -gt $y ]; then
     echo "X is greater than Y"
-elif [[ $x -eq $y ]]; then
+elif [ $x -eq $y ]; then
     echo "X is equal to Y"
 fi
 

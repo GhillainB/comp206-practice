2e81c1643d9e928d81397bc006dd9a1a9d8fa5c0
GhillainB <143177015+GhillainB@users.noreply.github.com>
2026-09-22 14:50:46 -0400

    Create comparing-numbers.sh

---
+++ bash/hackerrank/comparing-numbers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read x
+read y
+if [[ $x -lt $y ]]; then
+    echo "X is less than Y"
+elif [[ $x -gt $y ]]; then
+    echo "X is greater than Y"
+elif [[ $x -eq $y ]]; then
+    echo "X is equal to Y"
+fi
+
+# Will try a SWITCH condition
+# Was keen to if structure

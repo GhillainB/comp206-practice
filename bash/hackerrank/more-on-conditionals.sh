d8955b8e98d83242c3bd7519bec658e502992649
GhillainB <143177015+GhillainB@users.noreply.github.com>
2026-09-24 22:43:03 -0400

    used switch/case for conditions

--- bash/hackerrank/more-on-conditionals.sh
+++ bash/hackerrank/more-on-conditionals.sh
@@ -10,16 +10,17 @@ if [ $x -ge 1 ] && [ $y -ge 1 ] && [ $z -ge 1 ] &&
 [ $x -le 1000 ] && [ $y -le 1000 ] && [ $z -le 1000 ] &&
 [ $((x+y)) -gt $z ] && [ $((x+z)) -gt $y ] && [ $((y+z)) -gt $z ]; then
     
-    if [ $x -eq $y ] && [ $y -eq $z ]; then
+    case $x,$y,$z in
+        $y,$z,$x)
         echo "EQUILATERAL"
-        
-    elif [ $x -eq $y ] || [ $x -eq $z ] || [ $y -eq $z ]; then
+        ;;
+        x=y | x=z | y=z)
         echo "ISOSCELES"
-    
-    else
+        ;;
+        *)
         echo "SCALENE"
-    
-    fi
+        ;;
+        esac
 
 fi  
 
